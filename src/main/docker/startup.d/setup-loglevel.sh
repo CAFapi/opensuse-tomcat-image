cf1b72e70fd2721a7cb39bb402facde26a7844e3
Rahul Kulkarni <rahul.kulkarni@hpe.com>
2018-05-02 17:53:46 -0500

    SCMOD-3356: Adding log4j2 to java.util.logging map to set the right log level. Also, making sure the default is set right to info and usage of CAF_LOG_LEVEL is case insensitive (so CAF_LOG_LEVEL=debug and DEBUG should work)

--- src/main/docker/startup.d/setup-loglevel.sh
+++ src/main/docker/startup.d/setup-loglevel.sh
@@ -14,12 +14,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 #
-# This script replaces the log level value in logging.properties with CAF_LOG_LEVEL
+# This script replaces the log level value in logging.properties if environment variable CAF_LOG_LEVEL is set
 #
+#Declare and initialize associative array. This is a mapping of log4j2 levels with that of java.util.logging
+declare -A LOG4J2TOJAVAmap=([FATAL]=SEVERE [ERROR]=SEVERE [WARN]=WARNING [DEBUG]=FINE [TRACE]=FINEST)
 if [ -n "${CAF_LOG_LEVEL}" ]
 then
-    echo "Replacing log level in $CATALINA_HOME/conf/logging.properties with provided environment variable CAF_LOG_LEVEL"
-    sed -i "/org\.apache\.catalina\.core\.ContainerBase\.\[Catalina\]\.\[localhost\]\.level =/ s/=.*/= ${CAF_LOG_LEVEL}/" $CATALINA_HOME/conf/logging.properties
+    #if CAF_LOG_LEVEL is not set right we default it to INFO
+    LOGLEVEL=INFO
+    if [ -n "${LOG4J2TOJAVAmap[${CAF_LOG_LEVEL^^}]}" ]
+    then
+        LOGLEVEL=${LOG4J2TOJAVAmap[${CAF_LOG_LEVEL^^}]}
+    fi
+    echo "Environment log level variable set CAF_LOG_LEVEL:${CAF_LOG_LEVEL}"
+    echo "Replacing log level in $CATALINA_HOME/conf/logging.properties with equivalent log level:${LOGLEVEL}"
+    sed -i "/org\.apache\.catalina\.core\.ContainerBase\.\[Catalina\]\.\[localhost\]\.level =/ s/=.*/= ${LOGLEVEL}/" $CATALINA_HOME/conf/logging.properties
 fi

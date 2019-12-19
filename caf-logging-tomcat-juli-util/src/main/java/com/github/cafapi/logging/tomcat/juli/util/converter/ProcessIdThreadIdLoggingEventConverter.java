bf8ea4e29190d63cb4c6174c2381f0fd6f2106bf
thilagasanthosh <thilagavathi.santhoshkumar@microfocus.com>
2019-12-19 09:26:48 +0000

    SCMOD-5774: comment updated

---
+++ caf-logging-tomcat-juli-util/src/main/java/com/github/cafapi/logging/tomcat/juli/util/converter/ProcessIdThreadIdLoggingEventConverter.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2017-2020 Micro Focus or one of its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.cafapi.logging.tomcat.juli.util.converter;
+
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.pattern.DynamicConverter;
+import com.github.cafapi.logging.common.ProcessAndThreadIdProvider;
+
+public final class ProcessIdThreadIdLoggingEventConverter extends DynamicConverter<LoggingEvent>
+{
+    @Override
+    public String convert(LoggingEvent event)
+    {
+         return ProcessAndThreadIdProvider.getId();
+    }
+}

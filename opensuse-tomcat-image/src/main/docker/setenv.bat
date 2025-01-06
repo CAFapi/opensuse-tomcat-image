921ab4f7cbd702d5cd6def9873162db6e027b9da
Michael Bryson <mbryson@opentext.com>
2025-01-06 16:15:15 +0000

    US993080: Release preparation (#119)

--- opensuse-tomcat-image/src/main/docker/setenv.bat
+++ opensuse-tomcat-image/src/main/docker/setenv.bat
@@ -1,5 +1,5 @@
 @REM
-@REM Copyright 2017-2024 Open Text.
+@REM Copyright 2017-2025 Open Text.
 @REM
 @REM Licensed under the Apache License, Version 2.0 (the "License");
 @REM you may not use this file except in compliance with the License.
